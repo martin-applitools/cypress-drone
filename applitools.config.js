b23a5e2ab0c94cd93398fd0c327f3bf0169ead10
martin-applitools <martin.kowalewski@applitools.com>
2020-10-08 10:58:19 -0500

    disable cypress debug mode, disable setting of batchID

--- applitools.config.js
+++ applitools.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     showLogs: false,
-    saveDebugData: true,
+    saveDebugData: false,
     apiKey: 'zu4BybAMbOeRPtYF2mWHTAjNJOiuRPcD5JOUWcBP8lU110',
     concurrency: 25,
     disableBrowserFetching: true,
@@ -10,7 +10,7 @@ module.exports = {
     enablePatterns: false,
     failCypressOnDiff: false,
     //batchName: 'CondeNast Build: ' + process.env.DRONE_BUILD_NUMBER,
-    batchId: process.env.DRONE_COMMIT_SHA,
+    //batchId: process.env.DRONE_COMMIT_SHA,
     matchLevel: 'Layout',
     browser: [
         //W3C Top5 Screen Resolutions through 10/19 https://www.w3counter.com/globalstats.php
