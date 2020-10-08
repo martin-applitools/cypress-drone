30a721416426a23cb0ae4d5a1589ee224fa75274
martin-applitools <martin.kowalewski@applitools.com>
2020-10-08 16:22:44 -0500

    Enable search

--- cypress/integration/examples/applitools.js
+++ cypress/integration/examples/applitools.js
@@ -4,9 +4,9 @@ const urls = ['https://www.allure.com/account/sign-in',
     //'https://www.allure.com/branded/article/alr/allure-editor-in-chief-michelle-lee-jbeauty-tokyo',
     'https://www.allure.com/account/sign-up',
     'https://www.allure.com/gallery/best-body-washes',
-    // 'https://www.allure.com/gallery/readers-choice-skin-care-product-winners',
+    'https://www.allure.com/gallery/readers-choice-skin-care-product-winners',
     // 'https://www.allure.com/gallery/unique-halloween-costume-ideas',
-    'https://www.allure.com/search'
+    //'https://www.allure.com/search'
     // 'https://www.allure.com/sponsored/article/native-article-branded-content-by-with-text',
     // 'https://www.allure.com/sponsored/story/how-the-dyson-corrale-straighteners-flexing-plates-cause-less-damage-to-your-hair',
     // 'https://www.allure.com/story/sunday-riley-united-airlines-skin-care-in-flight-amenity-kits',
