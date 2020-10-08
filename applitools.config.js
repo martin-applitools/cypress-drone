129c3fd593fde17bfcf30d42472e406d8fea109c
martin-applitools <martin.kowalewski@applitools.com>
2020-10-08 10:54:03 -0500

    enable save debug data

--- applitools.config.js
+++ applitools.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     showLogs: false,
-    saveDebugData: false,
+    saveDebugData: true,
     apiKey: 'zu4BybAMbOeRPtYF2mWHTAjNJOiuRPcD5JOUWcBP8lU110',
     concurrency: 25,
     disableBrowserFetching: true,
