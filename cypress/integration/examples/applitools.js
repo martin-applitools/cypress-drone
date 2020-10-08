f0f870b29eca5b0cc13be2b198cbfa53d4c620a1
martin-applitools <martin.kowalewski@applitools.com>
2020-10-08 16:07:01 -0500

    Fix Typo in Tag, Enable 4 URLs

--- cypress/integration/examples/applitools.js
+++ cypress/integration/examples/applitools.js
@@ -3,16 +3,16 @@
 const urls = ['https://www.allure.com/account/sign-in',
     'https://www.allure.com/branded/article/alr/allure-editor-in-chief-michelle-lee-jbeauty-tokyo',
     'https://www.allure.com/account/sign-up',
-    'https://www.allure.com/gallery/best-body-washes',
-    'https://www.allure.com/gallery/readers-choice-skin-care-product-winners',
-    'https://www.allure.com/gallery/unique-halloween-costume-ideas',
-    'https://www.allure.com/search',
-    'https://www.allure.com/sponsored/article/native-article-branded-content-by-with-text',
-    'https://www.allure.com/sponsored/story/how-the-dyson-corrale-straighteners-flexing-plates-cause-less-damage-to-your-hair',
-    'https://www.allure.com/story/sunday-riley-united-airlines-skin-care-in-flight-amenity-kits',
-    'https://www.allure.com/story/wear-orange-national-gun-violence-awareness-day',
-    'https://www.allure.com/topic/love',
-    'https://www.allure.com/video/watch/jessica-alba-organic-vanilla-sugar-body-scrub']
+    'https://www.allure.com/gallery/best-body-washes']
+    // 'https://www.allure.com/gallery/readers-choice-skin-care-product-winners',
+    // 'https://www.allure.com/gallery/unique-halloween-costume-ideas',
+    // 'https://www.allure.com/search',
+    // 'https://www.allure.com/sponsored/article/native-article-branded-content-by-with-text',
+    // 'https://www.allure.com/sponsored/story/how-the-dyson-corrale-straighteners-flexing-plates-cause-less-damage-to-your-hair',
+    // 'https://www.allure.com/story/sunday-riley-united-airlines-skin-care-in-flight-amenity-kits',
+    // 'https://www.allure.com/story/wear-orange-national-gun-violence-awareness-day',
+    // 'https://www.allure.com/topic/love',
+    // 'https://www.allure.com/video/watch/jessica-alba-organic-vanilla-sugar-body-scrub']
 describe('Should Visually Validate Conde Nast WebSite and Brands', () => {
 
         it(`Should take a full page screenshot of URL`, () => {
@@ -24,7 +24,7 @@ describe('Should Visually Validate Conde Nast WebSite and Brands', () => {
                 });
                 cy.visit(urls[i]);
                 cy.eyesCheckWindow({
-                     tag: [i],
+                     tag: urls[i],
                      target: 'window',
                      fully: true
                  })
