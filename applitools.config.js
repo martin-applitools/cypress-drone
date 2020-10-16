b8560dfc999db876fa815f09dc3d5b83998eca82
martin-applitools <58984344+martin-applitools@users.noreply.github.com>
2020-10-16 09:45:17 -0500

    Update applitools.config.js

--- applitools.config.js
+++ applitools.config.js
@@ -1,6 +1,5 @@
 module.exports = {
     saveDebugData: false,
-    apiKey: 'zu4BybAMbOeRPtYF2mWHTAjNJOiuRPcD5JOUWcBP8lU110',
     dontCloseBatches: true,
     batchId: process.env.DRONE_COMMIT_SHA,
     disableBrowserFetching: true,
